4cb0941d3447aef4aa960f007deb926ff2bb86e6
Matt Huszagh <huszaghmatt@gmail.com>
2021-10-17 08:47:34 -0700

    fix lsp configuration for c modes

--- config/c-lsp-mode.el
+++ config/c-lsp-mode.el
@@ -19,6 +19,11 @@
 (setq lsp-auto-guess-root t)
 (setq lsp-idle-delay 0.5)
 
+;; disable installation of absent servers
+(add-hook 'lsp-mode-hook
+          (lambda () (mapc (lambda (client) (setf (lsp-client-download-server-fn client) nil))
+                           (ht-values lsp-clients))))
+
 ;; nix
 (add-to-list 'lsp-language-id-configuration '(nix-mode . "nix"))
 (lsp-register-client
@@ -27,8 +32,15 @@
                   :server-id 'nix))
 
 ;; c
+;; TODO what is the difference between these?
+;; These need to be set before loading LSP.
+(setq lsp-clangd-binary-path "clangd")
+(setq lsp-clients-clangd-executable "clangd")
+
+(require 'lsp-clangd)
 (add-hook 'c-mode-common-hook 'lsp)
-(setq lsp-clients-clangd-args '("--all-scopes-completion"
+
+(setq lsp-clients-clangd-args `("--all-scopes-completion"
                                 ;; index in background
                                 "--background-index"
                                 ;; use clang-tidy
@@ -39,12 +51,13 @@
                                 ;; suggest missing headers
                                 "--suggest-missing-includes"
                                 ;; use 8 cores
-                                "-j=8"
+                                ,(concat "-j=" (substring (shell-command-to-string "nproc") nil -1))
                                 ;; store PCH in memory for better performance
-                                "--pch-storage=memory"
-	                        "--clang-tidy-checks=-*,clang-analyzer-*"
-                                ",cert-*,bugprone-*,performance-*,portability-*"
-                                ",readability-*,-clang-analyzer-cplusplus*"))
+                                "--pch-storage=memory"))
+;; TODO some of this may be useful
+;; (concat "--clang-tidy-checks=-*,clang-analyzer-*"
+;;         ",cert-*,bugprone-*,performance-*,portability-*"
+;;         ",readability-*,-clang-analyzer-cplusplus*")
 
 (provide 'c-lsp-mode)
 
