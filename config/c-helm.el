d54bdb52f41a9f27f0a4f815097fb96ebee39514
Matt Huszagh <huszaghmatt@gmail.com>
2021-10-28 14:04:00 -0700

    use custom-set-variables for several helm settings

--- config/c-helm.el
+++ config/c-helm.el
@@ -29,8 +29,7 @@
       helm-ff-search-library-in-sexp t
       ;; scroll 8 lines other window using M-<next>/M-<prior>
       helm-scroll-amount 8
-      helm-ff-file-name-history-use-recentf t
-      helm-echo-input-in-header-line t)
+      helm-ff-file-name-history-use-recentf t)
 (setq helm-autoresize-max-height 0
       helm-autoresize-min-height 30)
 (helm-autoresize-mode 1)
@@ -39,5 +38,16 @@
 (setq helm-buffer-max-length 60)
 (setq-default helm-follow-mode-persistent t)
 
+(custom-set-variables
+ ;; TODO this does not fully remove the headline. I suspect this
+ ;; prevents the headline from scrolling, though I'm not really
+ ;; sure. I'd like to remove the headline alltogether.
+ '(helm-display-header-line nil)
+ ;; The input should appear at the bottom of the window (i.e., normal
+ ;; minibuffer location). I feel that placing the input at the top of
+ ;; the helm buffer is counterintuitive because it makes it appear as
+ ;; though the input is one of the selections from the list, which it
+ ;; isn't.
+ '(helm-echo-input-in-header-line nil))
 (provide 'c-helm)
 ;;; c-helm.el ends here
