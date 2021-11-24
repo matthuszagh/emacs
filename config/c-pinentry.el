026cc76c47349994aad337a62178bac37f33a623
Matt Huszagh <huszaghmatt@gmail.com>
2021-11-24 07:39:18 -0800

    change epa-pinentry-mode to epg-pinentry-mode

--- config/c-pinentry.el
+++ config/c-pinentry.el
@@ -9,7 +9,7 @@
 
 (require 'pinentry)
 ;; prompt gpg passphrase in minibuffer instead of in new window.
-(setq epa-pinentry-mode 'loopback)
+(setq epg-pinentry-mode 'loopback)
 
 (provide 'c-pinentry)
 ;;; c-pinentry.el ends here
