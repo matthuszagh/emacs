8ce748726933fdedb697cf1b8dad6767dfd8cb46
Matt Huszagh <huszaghmatt@gmail.com>
2021-11-24 07:35:58 -0800

    c-python-docstring: no double space between periods

--- config/c-python-docstring.el
+++ config/c-python-docstring.el
@@ -11,5 +11,8 @@
 
 (add-hook 'python-mode-hook #'python-docstring-mode)
 
+;; don't end sentences with double spaces
+(custom-set-variables '(python-docstring-sentence-end-double-space nil))
+
 (provide 'c-python-docstring)
 ;;; c-python-docstring.el ends here
