80dc5f8137e2f3c2394112775a127d895ae7bc68
Matt Huszagh <huszaghmatt@gmail.com>
2021-12-31 07:07:36 -0800

    c-helm-librarian.el: function to navigate to catalog entry for resource

--- config/c-helm-librarian.el
+++ config/c-helm-librarian.el
@@ -14,6 +14,17 @@
 (setq librarian-executable "~/src/librarian/target/release/librarian")
 (setq librarian-library-directory "~/doc/library")
 
+;; TODO this should be located somewhere else, since it doesn't
+;; actually relate to helm-librarian.
+(defun mh/catalog-entry ()
+  "Navigate to the catalog entry for a given resource."
+  (interactive)
+  (let ((filename (buffer-name))
+        (catalog-file (concat librarian-library-directory "/catalog.json")))
+    (find-file-other-window catalog-file)
+    (goto-char 0)
+    (search-forward filename)))
+
 (provide 'c-helm-librarian)
 
 ;;; c-helm-librarian.el ends here
