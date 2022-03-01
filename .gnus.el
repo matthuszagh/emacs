1d9c5d49b1adca5556976fe3f4983d774f83b9d6
Matt Huszagh <huszaghmatt@gmail.com>
2022-03-01 08:57:28 -0800

    update gnus to use custom-set-variables

--- .gnus.el
+++ .gnus.el
@@ -8,19 +8,29 @@
 ;; Save email locally.
 (require 'gnus)
 
-(setq user-mail-address "huszaghmatt@gmail.com"
-      user-full-name "Matt Huszagh")
-
-(setq nnmail-split-fancy
-      '(| (any "linux-kernel@vger\\.kernel\\.org" "LKML")
-          (any "emacs-devel@gnu\\.org" "emacs-devel")
-          (any "kicad-developers@lists\\.launchpad\\.net" "kicad-developers")
-          (any "emacs-orgmode@gnu\\.org" "emacs-orgmode")
-          "mail.misc"))
-
-(setq nnmail-split-methods 'nnmail-split-fancy)
-
-(setq gnus-select-method '(nntp "news.gmane.io"))
+(custom-set-variables
+ '(user-mail-address "huszaghmatt@gmail.com")
+ '(user-full-name "Matt Huszagh")
+ '(nnmail-split-fancy
+   '(| (any "linux-kernel@vger\\.kernel\\.org" "LKML")
+       (any "emacs-devel@gnu\\.org" "emacs-devel")
+       (any "kicad-developers@lists\\.launchpad\\.net" "kicad-developers")
+       (any "emacs-orgmode@gnu\\.org" "emacs-orgmode")
+       "mail.misc"))
+ '(nnmail-split-methods 'nnmail-split-fancy)
+ '(gnus-select-method '(nntp "news.gmane.io"))
+ '(gnus-secondary-select-methods '((nnml "")))
+ '(message-send-mail-function 'smtpmail-send-it)
+ '(smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil)))
+ '(smtpmail-auth-credentials '("smtp.gmail.com" 587 user-email-address nil))
+ '(smtpmail-default-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 587)
+ '(starttls-use-gnutls t)
+ ;; make Gnus NOT ignore [Gmail] mailboxes
+ '(gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+ ;; replace [ and ] with _ in ADAPT file names
+ '(nnheader-file-name-translation-alist '((?[ . ?_) (?] . ?_))))
 
 ;; (setq gnus-select-method
 ;;       '(nnimap "imap.gmail.com"
@@ -28,22 +38,6 @@
 ;;                (nnimap-split-methods "default")
 ;;                (nnimap-stream ssl)
 ;;                (nnimap-server-port 993)))
-
-(setq gnus-secondary-select-methods '((nnml "")))
-
-(setq message-send-mail-function 'smtpmail-send-it
-      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials '(("smtp.gmail.com" 587 user-email-address nil))
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587
-      starttls-use-gnutls t)
-
-;; Make Gnus NOT ignore [Gmail] mailboxes
-(setq gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
-
-;; Replace [ and ] with _ in ADAPT file names
-(setq nnheader-file-name-translation-alist '((?[ . ?_) (?] . ?_)))
 
 (provide '.gnus)
 ;;; .gnus.el ends here
