5db07bf12eba288b6413f039fda614178f7cc58f
Matt Huszagh <huszaghmatt@gmail.com>
2022-03-09 20:47:40 -0800

    update verilog-mode config

--- config/c-verilog-mode.el
+++ config/c-verilog-mode.el
@@ -4,33 +4,17 @@
 
 ;;; Code:
 
-(use-package verilog-mode
-  :mode "\\.[st]*v[hp]*\\'"
-  ;; :hook ((lambda ()
-  ;;          (setq-local paragraph-start "[ 	]*\\(//+\\|\\**\\)[ 	]*$\\|^\f")))
-  :bind (:map verilog-mode-map
-         ("C-c C-f" . indent-buffer))
-  :config
-  ;; Turns off automatic newline after typing a semicolon, which is annoying.
-  (setq verilog-auto-newline nil
-        verilog-indent-level 3
-        verilog-indent-level-declaration 3
-        verilog-indent-level-behavioral 3
-        verilog-indent-level-module 3
-        verilog-auto-lineup 'all
-	verilog-align-ifelse t
-	verilog-highlight-modules t
-	verilog-auto-read-includes t
-        verilog-case-indent 0
-        indent-tabs-mode t
-        verilog-auto-delete-trailing-whitespace t))
+(require 'verilog-mode)
 
-;; disable end of block comments. Although superficially nice, these
-;; are difficult to keep in sync with the code
-(setq verilog-auto-endcomments nil)
-
-;; use verilator for linting
-(setq verilog-linter "verilator –lint-only")
+(custom-set-variables
+ '(verilog-indent-level 4)
+ '(verilog-indent-level-module 4)
+ '(verilog-indent-level-declaration 4)
+ '(verilog-indent-level-behavioral 4)
+ '(verilog-auto-lineup nil)
+ '(c-basic-offset 4)
+ '(verilog-auto-endcomments nil)
+ '(verilog-linter "verilator –lint-only"))
 
 (provide 'c-verilog-mode)
 ;;; c-verilog-mode.el ends here
