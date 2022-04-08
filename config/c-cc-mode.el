5616526344313730448eccacebc8e46b98bc78bd
Matt Huszagh <huszaghmatt@gmail.com>
2022-04-08 07:58:56 -0700

    remove use-package in c-cc-mode.el

--- config/c-cc-mode.el
+++ config/c-cc-mode.el
@@ -7,21 +7,20 @@
 (if (featurep 'straight)
     (straight-use-package 'cc-mode))
 
-(use-package cc-mode
-  ;; :after (company company-c-headers helm)
-  :mode (("\\.c\\'" . c-mode)
-         ("\\.cc\\'" . c++-mode)
-         ("\\.cpp\\'" . c++-mode)
-         ("\\.tpp\\'" . c++-mode))
-  :hook ((c-mode-common . (lambda ()
-                            (add-hook 'before-save-hook 'clang-format-buffer nil t)))
-         ;; (c-mode-common . (lambda ()
-         ;;                    (set (make-local-variable 'company-backends)
-         ;;                         (list '(company-c-headers company-rtags company-files)))))
-         )
-  :config
-  (setq tab-width 8)
-  (setq c-default-style '((java-mode . "java") (awk-mode . "awk") (other . "linux"))))
+(require 'cc-mode)
+
+(cl-pushnew '("\\.c\\'" . c-mode) auto-mode-alist)
+(cl-pushnew '("\\.cc\\'" . c++-mode) auto-mode-alist)
+(cl-pushnew '("\\.cpp\\'" . c++-mode) auto-mode-alist)
+(cl-pushnew '("\\.tpp\\'" . c++-mode) auto-mode-alist)
+
+(custom-set-variables
+ '(tab-width 8)
+ '(c-default-style '((java-mode . "java") (awk-mode . "awk") (other . "linux"))))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (add-hook 'before-save-hook 'clang-format-buffer nil t)))
 
 (provide 'c-cc-mode)
 
