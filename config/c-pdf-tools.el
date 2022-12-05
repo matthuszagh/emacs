999df4cc6218f85e82c4a07500507c17d5d5f542
Matt Huszagh <huszaghmatt@gmail.com>
2022-12-05 07:15:20 -0800

    c-pdf-tools: use fill-column customization for outline line wrapping

--- config/c-pdf-tools.el
+++ config/c-pdf-tools.el
@@ -18,37 +18,11 @@
   ;; do not limit max page/image size
   (setq pdf-view-max-image-width 100000)
 
-  ;; TODO remove if this pr accepted: https://github.com/politza/pdf-tools/pull/602
-  (defun mh/pdf-outline-noselect (&optional buffer)
-    "Create an PDF outline of BUFFER, but don't display it."
-    (save-current-buffer
-      (and buffer (set-buffer buffer))
-      (pdf-util-assert-pdf-buffer)
-      (let* ((pdf-buffer (current-buffer))
-             (pdf-file (pdf-view-buffer-file-name))
-             (pdf-window (and (eq pdf-buffer (window-buffer))
-                              (selected-window)))
-             (bname (pdf-outline-buffer-name))
-             (buffer-exists-p (get-buffer bname))
-             (buffer (get-buffer-create bname)))
-        (with-current-buffer buffer
-          (setq-local fill-column nil)
-          (unless buffer-exists-p
-            (when (= 0 (save-excursion
-                         (pdf-outline-insert-outline pdf-buffer)))
-              (kill-buffer buffer)
-              (error "PDF has no outline"))
-            (pdf-outline-buffer-mode))
-          (set (make-local-variable 'other-window-scroll-buffer)
-               pdf-buffer)
-          (setq pdf-outline-pdf-window pdf-window
-                pdf-outline-pdf-document (or pdf-file pdf-buffer))
-          (current-buffer)))))
-
-  (advice-add 'pdf-outline-noselect :override #'mh/pdf-outline-noselect)
-  ;; end TODO
-
   (setq-default pdf-view-display-size 'fit-page))
+
+(custom-set-variables
+ ;; disable line wrapping in PDF outlines
+ '(pdf-outline-fill-column nil))
 
 (defun mh//pdf-view--rotate (&optional counterclockwise-p page-p)
   "Rotate PDF 90 degrees. Requires pdftk to work.\n
