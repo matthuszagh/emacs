37f39f3aa825e34bd847233ac308c2812fafec1e
Matt Huszagh <huszaghmatt@gmail.com>
2023-04-07 09:17:57 -0700

    c-autoinsert.el: restrict elisp autoinsert to config files
    This was doing funny things with straight autoload files. Moreover,
    it's only supposed to be used for config files, I'm not sure why I
    didn't restrict it that way in the first place.

--- config/c-autoinsert.el
+++ config/c-autoinsert.el
@@ -10,22 +10,24 @@
 (setq auto-insert-query nil)
 (setq auto-insert-alist
       '((emacs-lisp-mode . (lambda ()
-                             (let* ((fname (file-name-nondirectory (buffer-file-name)))
-                                    (fname-no-extension (file-name-sans-extension fname)))
-                               (insert (concat
-                                        ";;; " fname " ---  -*- lexical-binding: t; -*-\n"
-                                        "\n"
-                                        ";;; Commentary:\n"
-                                        "\n"
-                                        ";;; Code:\n"
-                                        "\n"
-                                        "(if (featurep 'straight)\n"
-                                        "    (straight-use-package '" (substring fname-no-extension 2) "))\n"
-                                        "\n"
-                                        "(require '" (substring fname-no-extension 2) ")\n"
-                                        "\n"
-                                        "(provide '" fname-no-extension ")\n"
-                                        ";;; " fname " ends here")))))))
+                             (if (equal (file-name-directory (buffer-file-name))
+                                        (expand-file-name "~/.config/emacs/config/"))
+                                 (let* ((fname (file-name-nondirectory (buffer-file-name)))
+                                        (fname-no-extension (file-name-sans-extension fname)))
+                                   (insert (concat
+                                            ";;; " fname " ---  -*- lexical-binding: t; -*-\n"
+                                            "\n"
+                                            ";;; Commentary:\n"
+                                            "\n"
+                                            ";;; Code:\n"
+                                            "\n"
+                                            "(if (featurep 'straight)\n"
+                                            "    (straight-use-package '" (substring fname-no-extension 2) "))\n"
+                                            "\n"
+                                            "(require '" (substring fname-no-extension 2) ")\n"
+                                            "\n"
+                                            "(provide '" fname-no-extension ")\n"
+                                            ";;; " fname " ends here"))))))))
 
 (provide 'c-autoinsert)
 ;;; c-autoinsert.el ends here
