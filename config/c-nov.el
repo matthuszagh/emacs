62e4e318264c64c9902d8bbbeba2acde8f20825f
Matt Huszagh <huszaghmatt@gmail.com>
2023-08-11 21:36:57 -0700

    c-nov.el: remove use-package and simplify setup

--- config/c-nov.el
+++ config/c-nov.el
@@ -5,42 +5,15 @@
 ;;; Code:
 
 (if (featurep 'straight)
-    (straight-use-package 'nov))
+    (progn
+      ;; prerequisite for nov
+      (straight-use-package 'esxml)
+      (straight-use-package 'nov)))
 
-(use-package nov
-  :mode ("\\.epub\\'" . nov-mode)
-  :init
-  (defun mh/nov-font-setup ()
-    (face-remap-add-relative 'variable-pitch :family "Source Code Pro"
-                             :height 1.0))
-  :hook ((nov-mode . mh/nov-font-setup))
-  :config
-  (setq nov-text-width 70)
+(require 'nov)
 
-  ;; TODO get full justification working
-  (defun mh/nov-window-configuration-change-hook ()
-    (mh/nov-post-html-render-hook)
-    (remove-hook 'window-configuration-change-hook
-                 'mh/nov-window-configuration-change-hook
-                 t))
-
-  (defun mh/nov-post-html-render-hook ()
-    (if (get-buffer-window)
-        (let ((max-width (pj-line-width))
-              buffer-read-only)
-          (save-excursion
-            (goto-char (point-min))
-            (while (not (eobp))
-              (when (not (looking-at "^[[:space:]]*$"))
-                (goto-char (line-end-position))
-                (when (> (shr-pixel-column) max-width)
-                  (goto-char (line-beginning-position))
-                  (pj-justify)))
-              (forward-line 1))))
-      (add-hook 'window-configuration-change-hook
-                'mh/nov-window-configuration-change-hook
-                nil t)))
-  (add-hook 'nov-post-html-render-hook 'mh/nov-post-html-render-hook))
+(custom-set-variables
+ '(nov-variable-pitch nil))
 
 (provide 'c-nov)
 ;;; c-nov.el ends here
