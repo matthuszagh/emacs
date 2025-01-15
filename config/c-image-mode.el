ee6176081fa251393ef8d3169821e65c2a1dd133
Matt Huszagh <huszaghmatt@gmail.com>
2025-01-14 19:53:28 -0800

    mh/svg-replace-black-with-current-color: improve

--- config/c-image-mode.el
+++ config/c-image-mode.el
@@ -10,7 +10,8 @@
   "Replace black strokes and fills with currentColor in current SVG document."
   (interactive)
   (replace-string "#000000" "currentColor")
-  (replace-string "rgb(0%,0%,0%)" "currentColor"))
+  (replace-string "rgb(0%,0%,0%)" "currentColor")
+  (replace-string "rgb(0%, 0%, 0%)" "currentColor"))
 
 (provide 'c-image-mode)
 ;;; c-image-mode.el ends here
